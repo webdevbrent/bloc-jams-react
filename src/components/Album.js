b792e64112b39eafd5d40541143aeaf7136915b0
webdevbrent <webdeveloperbrent@gmail.com>
2018-02-12 19:27:34 -0500

    Added Formated Times, Seek Controls, and Volume Controls

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import albumData from './../data/albums';
 import PlayerBar from './PlayerBar';
 
@@ -6,78 +6,104 @@ class Album extends Component {
   constructor(props) {
     super(props);
 
-      const album = albumData.find( (album) => {
+    const album = albumData.find(album => {
       return album.slug === this.props.match.params.slug
     });
 
-      this.state = {
-        album: album,
-        currentSong: album.songs[0],
-        isPlaying: false
-      };
+    this.state = {
+      album: album,
+      currentSong: album.songs[0],
+      currentTim: 0,
+      duration: album.songs[0].duration,
+      isPlaying: false,
+      volume: 0.5
+    };
 
-      this.audioElement = document.createElement('audio');
-      this.audioElement.src = album.songs[0].audioSrc;
-
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
 
-  play() {
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    };
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  play(){
     this.audioElement.play();
-    this.setState({ isPlaying: true });
+    this.setState({isPlaying: true});
   }
 
-  pause() {
+  pause(){
     this.audioElement.pause();
-    this.setState({ isPlaying: false });
+    this.setState({isPlaying: false});
   }
 
-  setSong(song) {
+  setSong(song){
     this.audioElement.src = song.audioSrc;
-    this.setState({ currentSong: song });
+    this.setState({currentSong: song});
   }
 
-  handleSongClick(song) {
+  formatTime(time) {
+    return time ? `${Math.floor(time / 60)}:${Number(time % 60 / 100).toFixed(2).substr(2,3)}` : '-:--';
+  }
+
+  handleSongClick(song){
     const isSameSong = this.state.currentSong === song;
-    if (this.state.isPlaying && isSameSong) {
+    if(this.state.isPlaying && isSameSong){
       this.pause();
-    }else {
-      if (!isSameSong) { this.setSong(song); }
+    } else {
+      if(!isSameSong){ this.setSong(song) }
       this.play();
     }
   }
 
-  handlePrevClick() {
-    // Find the index of the current song
-    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song );
-    // Calculate new index by subtracting 1 but not going below the first index of 0
+  handlePrevClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
     const newIndex = Math.max(0, currentIndex - 1);
-    // Get the new song after calculations
     const newSong = this.state.album.songs[newIndex];
-    // Call set song ont he newly calculated index value
     this.setSong(newSong);
-    // Play the song
     this.play(newSong);
   }
 
-  handleNextClick() {
-    // Find the index of the current song
-    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song );
-    // Calculate new index by subtracting the length of the songs array by one then adding 1 to the current index
+  handleNextClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
     const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
-    // Get the new song after calculations
     const newSong = this.state.album.songs[newIndex];
-    // Call set song ont he newly calculated index value
     this.setSong(newSong);
-    // Play the song
-    this.play(newSong);
+    this.play();
   }
 
-  render() {
+  handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
+  }
+
+  handleVolumeChange(e) {
+    const newVolume = e.target.value;
+    this.audioElement.volume = newVolume;
+    this.setState({ volume: newVolume });
+  }
+
+  render (){
     return (
       <section className="album">
-        {this.props.match.params.slug} Album
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} />
+          <img src={this.state.album.albumCover} alt="" id="album-cover-art"/>
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
@@ -86,38 +112,43 @@ class Album extends Component {
         </section>
         <table id="song-list">
           <colgroup>
-            <col id="song-number-colum" />
-            <col id="song-title-colum" />
-            <col id="song-duration-colum" />
+            <col id="song-number-column"/>
+            <col id="song-title-column"/>
+            <col id="song-duration-column"/>
           </colgroup>
           <tbody>
-            {this.state.album.songs.map( (song, index) =>
+            {this.state.album.songs.map( (song, index) => 
               <tr className={song} key={index} onClick={() => this.handleSongClick(song)} >
+              <td className="song-number">{index+1}</td>
                 <td className="song-actions">
-                    <button>
-                      <span className="ion-play"></span>
-                    </button>
-                    <button>
-                      <span className="ion-pause"></span>
-                    </button>
-                </td>
-                <td className="song-index"><span className="song-number">{index+1}</span></td>
+                  <button>
+                    <span className="ion-play"></span>
+                  </button>
+                  <button>
+                  <span className="ion-pause"></span>
+                  </button>
+                </td>         
                 <td className="song-title">{song.title}</td>
-                <td className="song-duration">{song.duration}</td>
+                <td className="song-duration">{this.formatTime(song.duration)}</td>
               </tr>
             )}
           </tbody>
         </table>
-        <PlayerBar
-          isPlaying={this.state.isPlaying}
+        <PlayerBar 
+          isPlaying={this.state.isPlaying} 
           currentSong={this.state.currentSong}
+          currentTime={this.audioElement.currentTime}
+          duration={this.audioElement.duration}
+          formatTime={(t) => this.formatTime(t)}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
+          handleVolumeChange={(e) => this.handleVolumeChange(e)}
         />
       </section>
     );
   }
-}
+};
 
 export default Album;

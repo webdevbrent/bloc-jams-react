ba406a73fa6f3703467c12e0ebc2418acdb120cf
webdevbrent <webdeveloperbrent@gmail.com>
2018-02-17 17:15:07 -0500

    Added styles to application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { Route, Link} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import {Navbar, NavItem} from 'react-materialize';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <Link to='/'>Landing|</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
+      <div className="navbar-fixed">
+      <div className="nav-wrapper">
+        <Navbar className="navbar-fixed" brand='Bloc Jams' left>
+          <NavItem href="/">Home</NavItem>
+          <NavItem href="/library">Library</NavItem>
+        </Navbar>
+      </div>
+    </div>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import {Row, Col, Card} from 'react-materialize';
 
 const Landing = () => (
+  <div className="container">
   <section className="landing">
-    <h1 className="hero-title">Turn the music up!</h1>
-
-    <section className="selling-points">
-      <div className="point">
-        <h2 className="point-title">Choose your music</h2>
-        <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-      </div>
-      <div className="point">
-        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-        <p className="point-description">No arbitrary limits. No distractions.</p>
-      </div>
-      <div className="point">
-        <h2 className="point-title">Mobile enabled</h2>
-        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-      </div>
-    </section>
+  <h1 className="center-align">Turn The Music Up!</h1>
+  <img className="responsive-img" src="https://pixabay.com/get/e830b70f20f2083ed1534705fb0938c9bd22ffd41cb019499df6c07ea7/rock-1560869_1920.jpg" alt="" />
+    <Row>
+      <Col m={4} s={12}>
+        <Card className='red darken-4 center-align' textClassName='white-text' title='Choose your music'>
+        <p className="center-align">The world is full of music; why should you have to listen to music that someone else chose?</p>
+		    </Card>
+      </Col>
+      <Col m={4} s={12}>
+        <Card className='yellow darken-4 center-align' textClassName='white-text' title='Unlimited, streaming, ad-free'>
+        <p className="center-align">No arbitrary limits. No distractions.</p>
+		    </Card>
+      </Col>
+      <Col m={4} s={12}>
+        <Card className='blue darken-4 center-align' textClassName='white-text' title='Mobile enabled'>
+        <p className="center-align">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+		    </Card>
+      </Col>
+    </Row>
   </section>
+  </div>
 );
 
 export default Landing;
